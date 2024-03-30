36eeb64805962ade36e533cf69c7e400bc45aa76
muskan <muskaann.129@gmail.com>
2024-03-30 20:19:13 +0530

    update: menu for 'Anyone can reply' in Create thread modal

--- src/components/activity.jsx
+++ src/components/activity.jsx
@@ -1,10 +1,8 @@
-import { useState, useEffect } from "react";
-
 export default function Activities() {
 
   return (
-      <div classNameName="text-white flex overflow-scroll p-1 gap-3 font-bold sm:w-[90%] lg:justify-center lg:overflow-hidden">
-        <button classNameName="px-10 py-1 border border-neutral-700 rounded-xl focus:bg-white focus:text-black">
+      <div className="text-white flex overflow-scroll p-1 gap-3 font-bold sm:w-[90%] lg:justify-center lg:overflow-hidden">
+        <button className="px-10 py-1 border border-neutral-700 rounded-xl focus:bg-white focus:text-black">
           All
         </button>
         <button className="px-6 py-1 border border-neutral-700 rounded-xl focus:bg-white focus:text-black">
